6f51211ed2884a61fdba7753b5877108442123ca
Liborio Castañeda Valencia <ldcastaneda06@misena.edu.co>
2024-08-28 16:30:54 -0500

    Finalización del CF03

--- src/config/global.js
+++ src/config/global.js
@@ -198,7 +198,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/733402_CF03-DU.pdf',
       },
       {
         icono: 'fas fa-download',
